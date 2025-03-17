c5cc9ae5c442e79edf3b9e559fcf4aaf77ed070c
nikp123 <nikp123@e.email>
2025-03-17 17:38:19 +0100

    create zram on boot

--- aconfmgr/fixes/hibernate-and-zram.sh
+++ aconfmgr/fixes/hibernate-and-zram.sh
@@ -1,6 +1,9 @@
 CreateLink /etc/systemd/system/hibernate.target.requires/pre-hibernation.service /etc/systemd/system/pre-hibernation.service
 CreateLink /etc/systemd/system/sleep.target.requires/pre-hibernation.service /etc/systemd/system/pre-hibernation.service
 CreateLink /etc/systemd/system/systemd-hibernate.service.requires/pre-hibernation.service /etc/systemd/system/pre-hibernation.service
+CreateLink /etc/systemd/system/multi-user.target.requires/boot-zram.service /etc/systemd/system/boot-zram.service
+
+CopyFile /etc/systemd/system/boot-zram.service
 CopyFile /etc/systemd/system/pre-hibernation.service
 CopyFile /etc/systemd/system/systemd-logind.service.d/override.conf 755
 CopyFile /usr/lib/systemd/system-sleep/zram-hibernate-hook 755
